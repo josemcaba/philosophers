0a6120e7dae6faba24c0464d2cd66199a45bcab6
Jose M. Caballero (WSL:Ubuntu) <josemcaba@gmail.com>
2023-09-02 16:24:29 +0200

    Cambios menores

--- philo/controller.c
+++ philo/controller.c
@@ -6,7 +6,7 @@
 /*   By: jocaball <jocaball@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 13:55:34 by jocaball          #+#    #+#             */
-/*   Updated: 2023/09/02 11:52:24 by jocaball         ###   ########.fr       */
+/*   Updated: 2023/09/02 12:26:42 by jocaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,29 +23,25 @@ static void	check_dead(t_data *data, t_philo **philos)
 		pthread_mutex_lock(&(*philos)[i].black_hole_mtx);
 		if (now() >= (*philos)[i].black_hole)
 		{
-			pthread_mutex_lock(&data->print_mtx);
+			print_state("died", &(*philos)[i]);
 			data->over = 1;
-			printf("%ld %d %s\n", now(), (*philos)[i].id, "died");
-			pthread_mutex_unlock(&data->print_mtx);
 		}
 		pthread_mutex_unlock(&(*philos)[i].black_hole_mtx);
 	}
 }
 
-static void    check_full(t_data *data)
+static void	check_full(t_data *data)
 {
 	if (data->over)
 		return ;
 	pthread_mutex_lock(&data->full_philos_mtx);
 	if (data->full_philos == data->nbr_philos)
 	{
-		//pthread_mutex_lock(&data->over_mtx);
 		pthread_mutex_lock(&data->print_mtx);
 		data->over = 1;
 		printf("%ld All philosophers have eaten at least %d times\n", \
 				now(), data->min_meals);
 		pthread_mutex_unlock(&data->print_mtx);
-		//pthread_mutex_unlock(&data->over_mtx);
 	}
 	pthread_mutex_unlock(&data->full_philos_mtx);
 }
