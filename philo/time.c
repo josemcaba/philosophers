ef463e82e51a4e134b9e436b86d9f7e609fb9e87
Jose M. Caballero (WSL:Ubuntu) <josemcaba@gmail.com>
2023-09-03 20:50:44 +0200

    VERSION 1.0 (ubuntu)

--- philo/time.c
+++ philo/time.c
@@ -6,7 +6,7 @@
 /*   By: jocaball <jocaball@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 14:30:21 by jocaball          #+#    #+#             */
-/*   Updated: 2023/09/03 17:19:44 by jocaball         ###   ########.fr       */
+/*   Updated: 2023/09/03 20:27:21 by jocaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	ft_wait(long msec, t_data *data)
 	while ((now() - start_time < msec) && !data->over)
 	{
 		pthread_mutex_unlock(&data->over_mtx);
-		usleep(1);
+		usleep(10);
 		pthread_mutex_lock(&data->over_mtx);
 	}
 	pthread_mutex_unlock(&data->over_mtx);
